a36ecb34b9eda9fa2b0be46bfd99aa0030083792
Arxae <maesenandy@gmail.com>
2015-05-21 00:17:46 +0200

    Added watch exampel

---
+++ Examples/Pages/5WatchPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BenhartLog;
+
+namespace Examples.Pages
+{
+	/// <summary>
+	/// Interaction logic for _5WatchPage.xaml
+	/// </summary>
+	public partial class _5WatchPage : Page
+	{
+		public _5WatchPage()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnSetUpdateWatch_Click(object sender, RoutedEventArgs e)
+		{
+			Benhart.SetWatch(TxtWatchName.Text, TxtWatchValue.Text);
+		}
+	}
+}
